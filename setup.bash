3c22cf1eff196c72e4bd465be184b6ce89bf2d66
QuintenVervynck <quinten.vervynck@gmail.com>
2020-08-19 14:12:09 +0200

    initial commit

---
+++ setup.bash
@@ -0,0 +1,10 @@
+ssh pi@192.168.0.229
+
+sudo apt update 
+sudo apt install python3
+sudo apt install python3-pip
+python3 -m pip install -U discord.py
+pip3 install -U selenium
+pip3 install webdriver_manager
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
