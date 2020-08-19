bb0f35f4d14b21a2c39de80afb1492a72c54c98e
quintenvervynck <vervynck.quinten@gmail.com>
2020-08-19 13:04:53 +0000

    added

--- setup.bash
+++ setup.bash
@@ -6,8 +6,12 @@ sudo apt install python3-pip
 # install python modules
 pip3 install -U discord.py
 pip3 install -U selenium
-pip3 install webdriver_manager
+pip3 install -U spotipy
+pip3 install -U webdriver_manager
 
 # install google-chrome (used by the webdriver_manager)
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
+sudo apt-get install libappindicator3-1
+
 sudo apt install ./google-chrome-stable_current_amd64.deb
