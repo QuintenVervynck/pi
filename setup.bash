f57e0e06ed98db6fdcd47d7b712c647498b20813
quintenvervynck <vervynck.quinten@gmail.com>
2020-08-19 12:18:05 +0000

    comments added

--- setup.bash
+++ setup.bash
@@ -1,10 +1,13 @@
-ssh pi@192.168.0.229
+sudo apt update
 
-sudo apt update 
+# install python and pip
 sudo apt install python3
 sudo apt install python3-pip
-python3 -m pip install -U discord.py
+# install python modules
+pip3 install -U discord.py
 pip3 install -U selenium
 pip3 install webdriver_manager
+
+# install google-chrome (used by the webdriver_manager)
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install ./google-chrome-stable_current_amd64.deb
