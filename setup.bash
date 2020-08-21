c3d3119e09d5f9eed43f5d9623155d3e8e1bcd7a
QuintenVervynck <quinten.vervynck@gmail.com>
2020-08-21 12:23:16 +0200

    a

--- setup.bash
+++ setup.bash
@@ -1,8 +1,12 @@
+
 sudo apt update
+
+mkdir projects
 
 # install python and pip
 sudo apt install python3
 sudo apt install python3-pip
+
 # install python modules
 pip3 install -U discord.py
 pip3 install -U selenium
@@ -11,5 +15,5 @@ pip3 install -U webdriver_manager
 
 # install google-chrome (used by the webdriver_manager)
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install gnome-session gnome-terminal 
 sudo apt install ./google-chrome-stable_current_amd64.deb
+# sudo apt install chromium-browser
