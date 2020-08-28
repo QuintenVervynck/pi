c9c10f828e674090f8a4edffccbabfc8cc4fe167
QuintenVervynck <quinten.vervynck@gmail.com>
2020-08-28 12:33:54 +0200

    menu website setup

---
+++ menu_website_sql_setup.bash
@@ -0,0 +1,12 @@
+sudo apt install postgresql libpq-dev -y
+sudo apt-get install python3-dev
+sudo pip install psycopg2
+
+
+# sudo -u postgres psql
+# CREATE DATABASE menu;
+# \q
+# sudo -u postgres psql postgres
+# \password postgres
+
+
