abdee7830102afcda055cb7d855f20e698de2b11
quintenvervynck <vervynck.quinten@gmail.com>
2020-08-28 13:16:52 +0200

    menu website db setup completed"

--- menu_website_sql_setup.bash
+++ menu_website_sql_setup.bash
@@ -10,3 +10,12 @@ sudo pip install psycopg2
 # \password postgres
 
 
+# Edit the PostgreSQL config file /etc/postgresql/9.6/main/postgresql.conf
+# to uncomment the listen_addresses line and change its value from 'localhost' to '*'. Save and exit.
+# sudo nano /etc/postgresql/11/main/postgresql.conf
+
+# Edit the pg_hba config file /etc/postgresql/9.6/main/pg_hba.conf
+# to change 127.0.0.1/32 to 0.0.0.0/0 for IPv4 and ::1/128 to ::/0 for IPv6. Save and exit.
+# sudo nano /etc/postgresql/11/main/pg_hba.conf
+
+# Restart the PostgreSQL service: sudo service postgresql restart.
