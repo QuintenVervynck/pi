87bee46cce9ef802f377a17dfc570138491ee262
quintenvervynck <vervynck.quinten@gmail.com>
2020-12-26 19:56:32 +0100

    rm json -> uses sqlite

--- main/loop.bash
+++ main/loop.bash
@@ -1,52 +1,48 @@
 #!/bin/bash
 
-prcs="../main/processes.json"
+# database
+DB=$(find ./ -iname "processes.sqlite" -exec readlink -f {} \;)
+
+# main table
+MTB="prcs"
 
 # go into the applications dir, since this is where all the programs are
 cd applications/
 
 # put every pid back to 0, that way whenever we first start this,
 # every process that is supposed to be running wil start again (bc it will still be "online": true)
-sed -i 's/\("pid":\) [0-9]\+/\1 0/g' $prcs
-
-
-# start the infinity loop
+sqlite3 $DB "update $MTB set pid=0"
 
 
 while true; do
-
-	for dir in $(ls); do
-		if [[ $(jq < $prcs .$dir) != "null" ]];then # directory inside applications exists in the json
-
-# should be online
-			if [[ $(jq < $prcs .${dir}.active) == "true" ]];then
-
-				if [[ $(jq < $prcs .${dir}.pid) -eq 0 ]];then
-	# needs to be started
-					startfile=$(jq < $prcs .${dir}.startfile) # the file that needs to be ran
-					cd $dir
-					python3 ${startfile:1:-1} 1>>../logs/log_${dir} 2>> ../logs/log_${dir}_err & # startfile needs the surrounding quotes to be deleted
-					last_pid=$! # this is the process id
-					cd ..
-					# adjust json
-					replacement=$(echo "."${dir}".pid="$last_pid)
-					jq < $prcs ${replacement} > temp.json
-					mv temp.json $prcs
-				fi
-
-# should be offline
-			elif [[ $(jq < $prcs .${dir}.active) == "false" ]];then
-
-				if [[ ! $(jq < $prcs .${dir}.pid) -eq 0 ]];then
-	# needs to be stopped
-					kill $(jq < $prcs .${dir}.pid) # kill process
-					# adjust json
-					replacement=$(echo "."${dir}".pid=0")
-					jq < $prcs ${replacement} > temp.json
-                                        mv temp.json $prcs
-				fi
-			fi
-		fi
-	done
-	sleep 5
+  for dir in $(ls); do
+    # find application dir in database
+    exists=$(sqlite3 $DB
+    if [[ $(sqlite3 $DB "select exists(select * from $MTB where name='$dir' limit 1);") -eq 1 ]];then
+      # should it be active?
+      pid=$(sqlite3 $DB "select pid from $MTB where name='$dir';")
+      if [[ $(sqlite3 $DB "select active from $MTB where name='$dir';") -eq 1 ]];then
+        # is it active?
+        if [[ $pid -eq 0 ]];then
+          # lets start it up
+          startfile=$(sqlite3 $DB "select start from $MTB where name='$dir';")
+          cd $dir
+          python3 $startfile 1>> log_${dir} 2>> log_${dir}_err & # startfile needs the surrounding quotes to be deleted
+          last_pid=$! # this is the process id
+          cd ..
+          # update database
+          sqlite3 $DB "update $MTB set pid=$last_pid where name='$dir';"
+        fi
+      else
+        # is it active?
+        if [[ ! $pid -eq 0 ]];then
+          # lets stop it
+          kill $pid
+          # update database
+          sqlite3 $DB "update $MTB set pid=0 where name='$dir';"
+        fi
+      fi
+    fi
+  done
+  sleep 3
 done
